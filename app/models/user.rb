92c90c235c27f587fd9fe78410d6ec0fb2324a4e
yasukin <70310523+yasukin@users.noreply.github.com>
2020-10-13 15:03:18 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence:true do
+    validates :nickname
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'please set including both half-width letters and numbers' }
+  end
 end
